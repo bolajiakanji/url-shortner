742f3232f34f42dd031ebe0ddf622169fad07053
lukman bolaji <bolajiakanji21@gmail.com>
2024-06-30 09:57:10 -0700

    working on deployment settings

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb+srv://bolajiakanji21:Unde5085@2@bolaji.l6yuhxc.mongodb.net/?retryWrites=true&w=majority&appName=Bolaji/playground')
 .then(() => console.log('connected to mongodb'))
-.catch(err => console.errpr('could not connect to mongodb'))
+.catch(err => console.error('could not connect to mongodb'))
 const courseSchema = new mongoose.Schema({
     name: String,
     author: String
